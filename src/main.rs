8be311ace2f5b6fc881da9c05a26b63f502d6bfb
Hauke Mehrtens <hauke@hauke-m.de>
2020-09-28 23:11:29 +0200

    Read and show temperature
    This configures the ADC, reads the temperature from the sensor inside
    the chip and tries to show it on the display.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,81 @@
 
 use panic_halt as _;
 
-use embedded_graphics::fonts::{Font6x8, Text};
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::Rectangle;
-use embedded_graphics::{primitive_style, text_style};
+use embedded_graphics::{primitive_style};
 use gd32vf103xx_hal::delay::McycleDelay;
 use gd32vf103xx_hal::pac;
 use gd32vf103xx_hal::prelude::*;
 use longan_nano::{lcd, lcd_pins};
 use riscv_rt::entry;
+
+fn enable_adc(adc: &pac::ADC0, delay: &mut McycleDelay) {
+    let rcu = unsafe { &*pac::RCU::ptr() };
+
+    /* adc_deinit(ADC0) */
+    rcu.apb2rst.modify(|_, w| w.adc0rst().set_bit());
+    rcu.apb2rst.modify(|_, w| w.adc0rst().clear_bit());
+
+    /* adc_mode_config(ADC_MODE_FREE) */
+    adc.ctl0.write(|w| unsafe { w.syncm().bits(0) });
+
+    /* adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE); */
+    adc.ctl0.modify(|_, w| w.sm().set_bit());
+
+    /* adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT); */
+    adc.ctl1.modify(|_, w| w.dal().clear_bit());
+
+    /* adc_tempsensor_vrefint_enable(); */
+    adc.ctl1.modify(|_, w| w.tsvren().set_bit());
+
+    /* adc_channel_length_config(ADC0, ADC_INSERTED_CHANNEL, 2); */
+    adc.isq.modify(|_, w| unsafe { w.il().bits(2 - 1) });
+
+    /* adc_inserted_channel_config(ADC0, 0, ADC_CHANNEL_16, ADC_SAMPLETIME_239POINT5); */
+    adc.isq.modify(|_, w| unsafe { w.isq3().bits(16) });
+    adc.sampt0.modify(|_, w| unsafe { w.spt16().bits(7) });
+
+    /* adc_inserted_channel_config(ADC0, 1, ADC_CHANNEL_17, ADC_SAMPLETIME_239POINT5); */
+    adc.isq.modify(|_, w| unsafe { w.isq2().bits(17) });
+    adc.sampt0.modify(|_, w| unsafe { w.spt17().bits(7) });
+
+    /* adc_external_trigger_source_config(ADC0, ADC_INSERTED_CHANNEL, ADC0_1_EXTTRIG_INSERTED_NONE); */
+    adc.ctl1.modify(|_, w| unsafe { w.etsic().bits(7) });
+
+    /* adc_external_trigger_config(ADC0, ADC_INSERTED_CHANNEL, ENABLE); */
+    adc.ctl1.modify(|_, w| w.eteic().set_bit());
+
+    /* adc_enable(ADC0); */
+    adc.ctl1.modify(|r, w| {
+        if r.adcon().bit_is_clear() {
+            w.adcon().set_bit()
+        } else {
+            w
+        }
+    });
+
+    delay.delay_ms(1);
+
+    /* adc_calibration_enable(ADC0); */
+    adc.ctl1.modify(|_, w| w.rstclb().set_bit());
+    while adc.ctl1.read().rstclb().bit_is_set() {}
+
+    adc.ctl1.modify(|_, w| w.clb().set_bit());
+    while adc.ctl1.read().clb().bit_is_set() {}
+}
+
+fn get_temp(adc: &pac::ADC0, delay: &mut McycleDelay) -> f32 {
+    /* adc_software_trigger_enable(ADC0, ADC_INSERTED_CHANNEL); */
+    adc.ctl1.modify(|_, w| w.swicst().set_bit());
+
+    delay.delay_ms(2000);
+
+    let idata0 = adc.idata0.read().idatan().bits() as f32;
+    let temp = (1.42 - idata0 * 3.3 / 4096.0) * 1000.0 / 4.3 + 25.0;
+    temp
+}
 
 #[entry]
 fn main() -> ! {
@@ -34,33 +99,23 @@ fn main() -> ! {
     let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
     let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
 
-    let style = text_style!(
-        font = Font6x8,
-        text_color = Rgb565::BLACK,
-        background_color = Rgb565::GREEN
-    );
-
     let mut delay = McycleDelay::new(&rcu.clocks);
-    let mut horizontal = 40;
-    let mut vertical = 35;
+
+    enable_adc(&dp.ADC0, &mut delay);
 
     loop {
+        let temp = get_temp(&dp.ADC0, &mut delay);
+
+        let position = (temp * 4.0) as i32;
         // Clear screen
         Rectangle::new(Point::new(0, 0), Point::new(width - 1, height - 1))
             .into_styled(primitive_style!(fill_color = Rgb565::BLACK))
             .draw(&mut lcd)
             .unwrap();
 
-        // Create a text at position (20, 30) and draw it using style defined above
-        Text::new(" Hello Rust! ", Point::new(horizontal, vertical))
-            .into_styled(style)
+        Rectangle::new(Point::new(0, 0), Point::new(position, height - 1))
+            .into_styled(primitive_style!(fill_color = Rgb565::RED))
             .draw(&mut lcd)
             .unwrap();
-
-        delay.delay_ms(250);
-        horizontal = horizontal + 1;
-        horizontal = horizontal % 80;
-        vertical = vertical + 1;
-        vertical = vertical % 70;
     }
 }
